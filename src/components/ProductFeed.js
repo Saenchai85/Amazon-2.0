0858c224d9882aa74a7ee392ec4fae199b5480ac
Biron <bironbenoit85@gmail.com>
2023-02-28 16:21:02 +0100

    ProductFeed implementation

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Product from './Product'
 
-function ProductFeed() {
+function ProductFeed({products}) {
   return (
-    <div>
-      <h1>Products here...</h1>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+      {products.map(({id, title, price, description, category, image}) => (
+        <Product 
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
     <div className="bg-gray-100 ">
       <Head>
@@ -13,9 +13,24 @@ export default function Home() {
       <Header />
 			<main className="max-w-screen-2xl mx-auto">
 				<Banner />
-				<ProductFeed />
+				<ProductFeed products={products} />
 			</main>
       
     </div>
   );
 }
+
+export async function getServerSideProps (context) {
+  const products = await fetch('https://fakestoreapi.com/products')
+    .then((res) => res.json()
+  )
+
+  return { 
+    props: {
+      products
+    }
+   }
+
+}
+
+// GET >>> https://fakestoreapi.com/products
